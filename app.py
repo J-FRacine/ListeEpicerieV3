b174442e9adda0e6ba5daf8e4cfae29bcd788309
J-FRacine <racine_jf@hotmail.com>
2026-07-22 14:07:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 from nicegui import ui
+from db import init_db
 
 ui.label('Hello from Canner!')
+
+# Initialiser la base
+init_db()
 
 port = int(os.getenv('PORT', 8080))
 ui.run(host='0.0.0.0', port=port)

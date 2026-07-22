d73cd916dafbb8536753948557816d36114bd0f9
J-FRacine <racine_jf@hotmail.com>
2026-07-22 16:47:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ init_db()
 # ---------------------------------------------------------
 current_family_id = None
 current_tab = 'items'
-tri_mode_items = 'Ordre d’ajout'
-tri_mode_needs = 'Ordre d’ajout'
+tri_mode_items = 'Alphabétique'
+tri_mode_needs = 'Alphabétique'
 
 
 # ---------------------------------------------------------

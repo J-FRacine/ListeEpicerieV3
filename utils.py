4ff48dda285d42e40929e934b0af4fffce321710
J-FRacine <racine_jf@hotmail.com>
2026-07-23 15:11:06 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
 from nicegui import ui
 from nicegui import app
+from db import get_categories
 
 def ensure_family_selected(current_family_id):
     """Affiche un message si aucune famille n'est sélectionnée."""
     if current_family_id is None:
         ui.label("⚠️ Aucune famille sélectionnée. Choisissez-en une dans l’onglet Familles.")
+        return False
+    return True
+
+def ensure_categories_exist():
+    """Affiche un message si aucune catégorie n'existe."""
+    categories = get_categories()
+    if not categories:
+        ui.label("⚠️ Aucune catégorie disponible. Ajoutez-en dans l’onglet Catégories.")
         return False
     return True
 
